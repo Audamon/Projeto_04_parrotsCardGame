98bf19b135ed3a57b1712895bea678f42e708b7e
João Alfredo Tamagno <joaotamagno@gmail.com>
2021-07-28 17:01:45 -0300

    The user can only input a even number between 4 and 14

--- Scripts/script.js
+++ Scripts/script.js
@@ -1,3 +1,21 @@
+let cardsQtd = Number(prompt("Com quantas cartas quer jogar?"));
+let counter = 0;
+
+while(cardsQtd < 4 || cardsQtd > 14 || (cardsQtd % 2) !== 0){
+    cardsQtd = Number(prompt("Com quantas cartas quer jogar?"));
+    
+}
+
+function gameStart(){
+    let gifList = ["bobrossparrot","explodyparrot", "fiestaparrot", "metalparrot", "revertitparrot", "tripletsparrot", "unicorparrot"];
+
+
+    let cardDeck = document.querySelector(".cards-container");
+
+    while(i < (cardsQtd / 2)){
+
+    }
+
 
 
 
